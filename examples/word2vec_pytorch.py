51c62647e31de31803c702e279edea3d103ed7d9
Jintao Zhang <jintao@squareup.com>
2020-12-28 13:02:22 -0800

    initial commit

---
+++ examples/word2vec_pytorch.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+
+
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from word2vec.data_reader import DataReader, Word2vecDataset
+from word2vec.model import SkipGramModel
+
+
+class Word2VecTrainer:
+    def __init__(self, input_file, output_file, emb_dimension=100, batch_size=32, window_size=5, iterations=3,
+                 initial_lr=0.001, min_count=12):
+
+        self.data = DataReader(input_file, min_count)
+        dataset = Word2vecDataset(self.data, window_size)
+        self.dataloader = DataLoader(dataset, batch_size=batch_size,
+                                     shuffle=False, num_workers=0, collate_fn=dataset.collate)
+
+        self.output_file_name = output_file
+        self.emb_size = len(self.data.word2id)
+        self.emb_dimension = emb_dimension
+        self.batch_size = batch_size
+        self.iterations = iterations
+        self.initial_lr = initial_lr
+        self.skip_gram_model = SkipGramModel(self.emb_size, self.emb_dimension)
+
+        self.use_cuda = torch.cuda.is_available()
+        self.device = torch.device("cuda" if self.use_cuda else "cpu")
+        if self.use_cuda:
+            self.skip_gram_model.cuda()
+
+    def train(self):
+
+        for iteration in range(self.iterations):
+
+            print("\n\n\nIteration: " + str(iteration + 1))
+            optimizer = optim.SparseAdam(self.skip_gram_model.parameters(), lr=self.initial_lr)
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(self.dataloader))
+
+            running_loss = 0.0
+            for i, sample_batched in enumerate(tqdm(self.dataloader)):
+
+                if len(sample_batched[0]) > 1:
+                    pos_u = sample_batched[0].to(self.device)
+                    pos_v = sample_batched[1].to(self.device)
+                    neg_v = sample_batched[2].to(self.device)
+
+                    scheduler.step()
+                    optimizer.zero_grad()
+                    loss = self.skip_gram_model.forward(pos_u, pos_v, neg_v)
+                    loss.backward()
+                    optimizer.step()
+
+                    running_loss = running_loss * 0.9 + loss.item() * 0.1
+                    if i > 0 and i % 500 == 0:
+                        print(" Loss: " + str(running_loss))
+
+            self.skip_gram_model.save_embedding(self.data.id2word, self.output_file_name)
+
+
+if __name__ == '__main__':
+    w2v = Word2VecTrainer(input_file="input.txt", output_file="out.vec")
+    w2v.train()
+
+
+"""
+    u_embedding: Embedding for center word.
+    v_embedding: Embedding for neighbor words.
+"""
+
+
+class SkipGramModel(nn.Module):
+
+    def __init__(self, emb_size, emb_dimension):
+        super(SkipGramModel, self).__init__()
+        self.emb_size = emb_size
+        self.emb_dimension = emb_dimension
+        self.u_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+        self.v_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+
+        initrange = 1.0 / self.emb_dimension
+        init.uniform_(self.u_embeddings.weight.data, -initrange, initrange)
+        init.constant_(self.v_embeddings.weight.data, 0)
+
+    def forward(self, pos_u, pos_v, neg_v):
+        emb_u = self.u_embeddings(pos_u)
+        emb_v = self.v_embeddings(pos_v)
+        emb_neg_v = self.v_embeddings(neg_v)
+
+        score = torch.sum(torch.mul(emb_u, emb_v), dim=1)
+        score = torch.clamp(score, max=10, min=-10)
+        score = -F.logsigmoid(score)
+
+        neg_score = torch.bmm(emb_neg_v, emb_u.unsqueeze(2)).squeeze()
+        neg_score = torch.clamp(neg_score, max=10, min=-10)
+        neg_score = -torch.sum(F.logsigmoid(-neg_score), dim=1)
+
+        return torch.mean(score + neg_score)
+
+    def save_embedding(self, id2word, file_name):
+        embedding = self.u_embeddings.weight.cpu().data.numpy()
+        with open(file_name, 'w') as f:
+            f.write('%d %d\n' % (len(id2word), self.emb_dimension))
+            for wid, w in id2word.items():
+                e = ' '.join(map(lambda x: str(x), embedding[wid]))
+                f.write('%s %s\n' % (w, e))
+
+
